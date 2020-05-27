106c06fa568c23324afbb3aa3d375ebd178cf210
Jaouad Hba <jaouadhba@gmail.com>
2020-05-28 00:55:05 +0200

    US 1 : code review add constant class

---
+++ src/main/java/org/jh/com/commun/Constant.java
@@ -0,0 +1,7 @@
+package org.jh.com.commun;
+
+public final class Constant {
+    private Constant(){}
+    public static final String THE_ACCOUNT_NOT_YET_INITIALIZED = "The Account not yet initialized";
+    public static final String THIS_OPERATION_CAN_T_BE_DONE = "This operation can't be done";
+}
